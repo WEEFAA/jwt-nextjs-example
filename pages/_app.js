90763ebe423a5e4930095be4ebfaaa281672c10e
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-17 18:07:52 +0800

    New: project init

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+function App({Component, props}){
+    return <>
+        <Head>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <Component {...props}/>
+    
+    </>
+}
+
+export default App
