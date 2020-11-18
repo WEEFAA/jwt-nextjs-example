78a4d08a4910145139daca75e4b0d40ba78e3e93
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-18 14:41:09 +0800

    New: added Error component

--- components/page.js
+++ components/page.js
@@ -1,7 +1,34 @@
-import { Container } from "@material-ui/core"
+import { Container, Snackbar } from "@material-ui/core"
+import MuiAlert from '@material-ui/lab/Alert';
+import { useEffect, useState } from "react"
 
 export const Page = ({ children }) => {
     return <Container component="main" maxWidth="sm">
         { children }
     </Container>
+}
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+export const Error = ({ message }) => {
+    const [open, setOpen] = useState(false)
+    
+    useEffect(() => {
+        setOpen(Boolean(message))
+    },[message])
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    }
+    
+    return <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">
+            { message || null }
+        </Alert>
+    </Snackbar>
 }
