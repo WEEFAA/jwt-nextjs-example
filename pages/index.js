eaaf82528c46132e87c9ef7d06ae9588f87ff8da
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-18 14:40:42 +0800

    Update: implemented login auth

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import { Page } from "../components/page"
-import { Typography, OutlinedInput, FormControl, InputLabel, Button, Container, Input } from "@material-ui/core"
+import { Error, Page } from "../components/page"
+import { Typography, OutlinedInput, FormControl, InputLabel, Button, Container, Input, formatMs } from "@material-ui/core"
 import { Buttons, Elevated, Form } from "../components/layout"
-import { useState, createRef, useRef } from "react"
+import { useState, createRef, useEffect } from "react"
+import { authLogin } from "../lib/auth"
 
 function Home(){
-    // state
+    // states
     const [fields, toggleFields] = useState({
         username: "",
         password: ""
     })
+    const [errors, setErrors] = useState({
+        user: false, // username & password error state
+        pass: false
+    })
+    const [message, setMessage] = useState("")
 
     const formRef = createRef()
     // form control
@@ -21,7 +27,7 @@ function Home(){
     }
 
     // submit handler
-    function onSubmit(){
+    async function onSubmit(){
         const {current: loginForm} = formRef
         // ref not established
         if(!loginForm) return null
@@ -30,7 +36,35 @@ function Home(){
         if(!valid) {
             return loginForm.reportValidity()
         }
-        loginForm.submit()
+        // authorize user 
+        await submitHandler()
+    }   
+    
+    async function submitHandler(){
+        // get form data
+        const formSrc = document.forms['loginForm']
+        const formData = new FormData(formSrc)
+        const authCredentials = {
+            username: formData.get('username') || "",
+            password: formData.get('password') || ""
+        }
+        // auth
+        const authenticator = authLogin(authCredentials)   
+        const authResult = await authenticator()
+        // handle result
+        authHandler(authResult)
+    }
+
+    function authHandler(result){
+        if(!result.ok){
+            // error message, if exist
+            setMessage(result.message)
+            // update error states
+            setErrors({
+                user: !!result.user,
+                pass: !!result.pass 
+            })
+        }
     }
 
     return <Page>
@@ -41,19 +75,20 @@ function Home(){
             <Typography variant="h6" component="h2" color="textSecondary" style={{textAlign: 'center'}}>
                 JWT authorization DEMO
             </Typography>
-            <Form ref={formRef} method="POST">
+            <Form ref={formRef} method="POST" action="/api/auth/login" >
+                <Error message={message}/>
                 <FormControl variant="outlined" fullWidth margin="normal" size="small" required>
                     <InputLabel htmlFor="username">Username</InputLabel>
                     <OutlinedInput 
                         id="username" name="username" 
-                        placeholder="emela" label="username"
+                        placeholder="emela" label="username" error={errors.user}
                         value={fields.username} onChange={onChange} required/>
                 </FormControl>
                 <FormControl variant="outlined" fullWidth size="small" required>
                     <InputLabel htmlFor="password">Password</InputLabel>
                     <OutlinedInput 
                         id="password" name="password" type="password"
-                        placeholder="************" label="password" 
+                        placeholder="************" label="password" error={errors.pass}
                         value={fields.password} onChange={onChange} required/>
                 </FormControl>
                 <Buttons>
