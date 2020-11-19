e84b65bcd9aff6c4ba023672a19f4fd35c8eee14
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-19 18:12:58 +0800

    uncontrolled implementation

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,19 @@ import { Error, Page } from "../components/page"
 import { Typography, OutlinedInput, FormControl, InputLabel, Button, Container, Input, formatMs } from "@material-ui/core"
 import { Buttons, Elevated, Form } from "../components/layout"
 import { useState, createRef, useEffect } from "react"
+import { useRouter } from "next/router"
 import { authLogin } from "../lib/auth"
 
 function Home(){
+    const router = useRouter()
     // states
     const [fields, toggleFields] = useState({
         username: "",
         password: ""
     })
-    const [errors, setErrors] = useState({
-        user: false, // username & password error state
-        pass: false
-    })
+    const [errors, setErrors] = useState([])
     const [message, setMessage] = useState("")
 
-    const formRef = createRef()
     // form control
     function onChange(e){
         const { target } = e 
@@ -26,46 +24,15 @@ function Home(){
         })
     }
 
-    // submit handler
-    async function onSubmit(){
-        const {current: loginForm} = formRef
-        // ref not established
-        if(!loginForm) return null
-        // submit the form
-        const valid = loginForm.checkValidity()
-        if(!valid) {
-            return loginForm.reportValidity()
-        }
-        // authorize user 
-        await submitHandler()
-    }   
-    
-    async function submitHandler(){
-        // get form data
-        const formSrc = document.forms['loginForm']
-        const formData = new FormData(formSrc)
-        const authCredentials = {
-            username: formData.get('username') || "",
-            password: formData.get('password') || ""
-        }
-        // auth
-        const authenticator = authLogin(authCredentials)   
-        const authResult = await authenticator()
-        // handle result
-        authHandler(authResult)
-    }
-
-    function authHandler(result){
-        if(!result.ok){
-            // error message, if exist
-            setMessage(result.message)
-            // update error states
-            setErrors({
-                user: !!result.user,
-                pass: !!result.pass 
-            })
-        }
-    }
+    useEffect(() => {
+        const query = router.query 
+        // check errors 
+        const { errors = "", message = "" } = query
+        const failing = errors.split(',') 
+        // set errors if exist
+        setErrors(failing)
+        setMessage(message)
+    },[router.query])
 
     return <Page>
         <Elevated>
@@ -75,24 +42,25 @@ function Home(){
             <Typography variant="h6" component="h2" color="textSecondary" style={{textAlign: 'center'}}>
                 JWT authorization DEMO
             </Typography>
-            <Form ref={formRef} method="POST" action="/api/auth/login" >
+            <Form method="POST" action="/api/auth/login" >
                 <Error message={message}/>
                 <FormControl variant="outlined" fullWidth margin="normal" size="small" required>
                     <InputLabel htmlFor="username">Username</InputLabel>
                     <OutlinedInput 
                         id="username" name="username" 
-                        placeholder="emela" label="username" error={errors.user}
+                        placeholder="emela" label="username" error={errors.includes('user')}
                         value={fields.username} onChange={onChange} required/>
                 </FormControl>
                 <FormControl variant="outlined" fullWidth size="small" required>
                     <InputLabel htmlFor="password">Password</InputLabel>
                     <OutlinedInput 
                         id="password" name="password" type="password"
-                        placeholder="************" label="password" error={errors.pass}
+                        placeholder="************" label="password" error={errors.includes('pass')}
                         value={fields.password} onChange={onChange} required/>
                 </FormControl>
                 <Buttons>
-                    <Button color="primary" variant="contained" onClick={onSubmit}>Login</Button>
+                    <Input type="submit" value="submit"/>
+                    <Button color="primary" variant="contained" >Login</Button>
                     <Button variant="contained">Info</Button>
                 </Buttons>
             </Form>
