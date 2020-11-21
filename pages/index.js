b86b1c2c024fedae3703494c7978940bf23dacb6
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-21 19:04:00 +0800

    Update: csrf implemented

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import { Typography, CircularProgress, Link } from "@material-ui/core"
-import { Elevated } from "../components/layout"
+import { Typography, CircularProgress, Link, Input, Box } from '@material-ui/core'
+import { Elevated, Form } from '../components/layout'
 import { Page } from "../components/page"
 import { useUser } from "../lib/auth"
+import { generateCsrf } from '../lib/csrf'
 
-const api = () => {
+const api = ({ _csrf }) => {
     // next router
     const { error, data, isValidating } = useUser()
     // check errors
@@ -26,13 +27,38 @@ const api = () => {
         user = ""
     } = data || {}
     // show authenticated user
-    return <Page>
-        <Elevated>
-            <Typography variant="h3" component="h1" color="primary">Welcome {user}</Typography>
-            <Typography variant="subtitle1" component="p" color="textSecondary" paragraph>you are now authroized, congratulations!</Typography>
-            <Link color="secondary" href="/logout">Log out</Link>
-        </Elevated>
-    </Page>
+    return (
+		<Page>
+			<Elevated>
+				<Typography variant="h3" component="h1" color="primary" align="center">
+					Welcome {user}
+				</Typography>
+				<Typography
+                    align="center"
+					variant="subtitle1"
+					component="p"
+					color="textSecondary"
+					paragraph>
+					you are now authorized, congratulations!
+				</Typography>
+                <img className="magic-meme" width={365} height={250} alt="A guy saying magic!" src="https://media1.tenor.com/images/d820481ef14b1cd2a16ff4e7660deb5f/tenor.gif?itemid=9239559"/>
+				<Form method="POST" action="/api/auth/logout">
+                    <Box display="flex" justifyContent="center">
+                        <Input type="submit" value="Logout" name="logout" disableUnderline/>
+                    </Box>
+					<Input type="hidden" name="_csrfToken" value={_csrf} />
+				</Form>
+			</Elevated>
+		</Page>
+	)
+}
+
+export const getServerSideProps = async function(context){
+    const { res } = context 
+    const _csrf = await generateCsrf(res)
+    return {
+        props: { _csrf }
+    }
 }
 
 export default api
