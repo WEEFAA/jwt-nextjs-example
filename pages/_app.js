4efb62e9fe589a27c48357cebc768d41cec8b957
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-21 21:05:41 +0800

    Fix default styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
-import Head from "next/head"
-import "./index.css"
+import './index.css'
 
 function App({Component, pageProps}){
-    return <>
-        <Head>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        </Head>
-        <Component {...pageProps}/>
-    
-    </>
+    return <Component {...pageProps} />
 }
 
 export default App
