2ebe01054c172bc518f99be752a4e23395a254f1
WEEFAA <rayranilfu7o7@gmail.com>
2020-11-21 17:51:54 +0800

    Fix wrong prop name

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import Head from "next/head"
 import "./index.css"
 
-function App({Component, props}){
+function App({Component, pageProps}){
     return <>
         <Head>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
-        <Component {...props}/>
+        <Component {...pageProps}/>
     
     </>
 }
