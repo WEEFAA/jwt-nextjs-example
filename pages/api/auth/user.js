86d24b3259aa1c2856f1ce5b957db0b01cc08642
Ael Festijo <aelfestijo@gmail.com>
2020-12-09 21:44:22 +0800

    Re-implement verifying of user state

--- pages/api/auth/user.js
+++ pages/api/auth/user.js
@@ -1,10 +1,22 @@
-import { verifyJWT } from "../../../lib/jwt"
+import { verifyUser } from '../../../lib/jwt'
+import { cookieName } from '../../../lib/jwt'
+import jwt from 'jsonwebtoken'
 
 const getUser = async (req,res) => {
     const { method } = req 
     switch(method){
         case "GET": {
-            return await verifyJWT(req, res)
+            const verified = await verifyUser(req)
+			if (!verified) return res.status(403).json({})
+
+			// decode the token
+			const token = req.cookies[cookieName]
+			const payload = jwt.decode(token)
+
+			return res.json({
+				...payload,
+				user: payload.sub,
+			})
         }
         default:
             return res.status(503).end()
