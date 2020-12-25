551ffea163cc4abe6bcd1fb75e49b14a5dedc4c9
Ael Festijo <aelfestijo@gmail.com>
2020-12-25 17:59:34 +0800

    refactor home page

--- pages/index.js
+++ pages/index.js
@@ -5,53 +5,52 @@ import { useUser } from "../lib/auth"
 import { generateCsrf } from '../lib/csrf'
 import Head from 'next/head'
 
+const Redirect_View = (
+	<Page>
+		<Head>
+			<title>Not allowed 👈🏻 JWT-NextJS</title>
+		</Head>
+		<Elevated>
+			<Typography
+				variant="h4"
+				align="center"
+				color="primary"
+				component="h1">
+				Not Authorize
+			</Typography>
+			<Typography component="p" variant="subtitle1" align="center">
+				please log in first
+			</Typography>
+			<img
+				className="r-banner"
+				width={365}
+				height={250}
+				alt="A guy suprised, laughing with himself!"
+				src="https://media.tenor.com/images/9d0da6776cd6c7a0551ab44dba355308/tenor.gif"
+			/>
+			<Box display="flex" justifyContent="center">
+				<Link
+					color="primary"
+					align="center"
+					href="/login"
+					variant="subtitle1">
+					Log in
+				</Link>
+			</Box>
+		</Elevated>
+	</Page>
+)
+
+
 const api = ({ _csrf }) => {
 	// next router
-	const { error, data, isLoading } = useUser()
+	const { data, isLoading } = useUser()
 	// check errors
 	if (isLoading) return <CircularProgress />
 
 	// redirect to login if not authorized
-	if (data && !data.user) {
-		return (
-			<Page>
-				<Head>
-					<title>Not allowed 👈🏻 JWT-NextJS</title>
-				</Head>
-				<Elevated>
-					<Typography
-						variant="h4"
-						align="center"
-						color="primary"
-						component="h1">
-						Not Authorize
-					</Typography>
-					<Typography
-						component="p"
-						variant="subtitle1"
-						align="center">
-						please log in first
-					</Typography>
-					<img
-						className="r-banner"
-						width={365}
-						height={250}
-						alt="A guy suprised, laughing with himself!"
-						src="https://media.tenor.com/images/9d0da6776cd6c7a0551ab44dba355308/tenor.gif"
-					/>
-					<Box display="flex" justifyContent="center">
-						<Link
-							color="primary"
-							align="center"
-							href="/login"
-							variant="subtitle1">
-							Log in
-						</Link>
-					</Box>
-				</Elevated>
-			</Page>
-		)
-	}
+	if (data && !data.user) return Redirect_View
+
 	const { user = '' } = data
 	// show authenticated user
 	return (
