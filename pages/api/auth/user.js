eaf9a021ebe9a1c6e642802a075a01b718639d0d
Ael Festijo <aelfestijo@gmail.com>
2021-01-05 13:32:00 +0800

    Fixed: fetch user info from the backend

--- pages/api/auth/user.js
+++ pages/api/auth/user.js
@@ -1,22 +1,35 @@
 import { verifyUser } from '../../../lib/jwt'
 import { cookieName } from '../../../lib/jwt'
 import jwt from 'jsonwebtoken'
+import axios from 'axios'
 
 const getUser = async (req,res) => {
     const { method } = req 
     switch(method){
         case "GET": {
             const verified = await verifyUser(req)
-			if (!verified) return res.status(403).json({})
+			if (!verified) return res.status(401).json({})
 
 			// decode the token
 			const token = req.cookies[cookieName]
             const payload = jwt.decode(token) 
-
-			return res.json({
-				...payload,
-				user: payload.id,
-			})
+            const backend_uri = process.env.BACKEND_URI || ""
+            const user_info_uri = `${backend_uri}/users/me`
+            const headers = {
+                Authorization: `Bearer ${token}`,
+            }
+            // try to fetch user info
+            try {
+				const response = await axios.get(user_info_uri,{ headers })
+                const { data } = response 
+                return res.json({
+                    ...payload,
+                    user: data.username,
+                })
+			} catch (e) {
+                // failed to get user information
+				res.status(401).end()
+			}
         }
         default:
             return res.status(503).end()
